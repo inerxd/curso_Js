06dfd2c14e2e04b0bc8ad211564a25e9ff438faa
javier <inerxd@gmail.com>
2024-07-20 21:55:03 -0600

    subida 2.1

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -4,21 +4,51 @@ document.addEventListener('DOMContentLoaded', function(){
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const formulario = document.querySelector('#formulario');
 
     inputEmail.addEventListener('blur',validar);
     inputAsunto.addEventListener('blur',validar);
     inputMensaje.addEventListener('blur',validar);
+    
 
     function validar(e){
-        console.log(e.target.value.trim())
-
-        if(e.target.value === ''){
-            console.log("no hay string")
-        }else{
-            console.log(' hay string')
+       console.log(e.target.id)
+        if(e.target.value.trim() === ''){
+            mostrarAlerta(`este campo ${e.target.id} es obligatorio`,e.target.parentElement);
+            return;
+            }
+     
+        if(e.target.id === 'email' && !validarEmail(e.target.value)){
+            mostrarAlerta('El email no es valido', e.target.parentElement);
+            return;
         }
+        limpiarAlerta(e.target.parentElement);
     };
 
+    function mostrarAlerta(mensaje,referencia){
+       
+        limpiarAlerta(referencia);
+        // general codigo html
+        const error = document.createElement('P');
+        error.textContent = mensaje;
+        error.classList.add('bg-red-600','text-white','p-2','text-center');
+        // inyectar el error al formulario
+        referencia.appendChild(error);
+
+    }
+    function limpiarAlerta(referencia){
+         // comprueba si ya existe una alerta
+        const alerta = referencia.querySelector('.bg-red-600')
+        if(alerta){
+            alerta.remove();
+        }
+    }
+
+    function validarEmail(email){
+        const regex =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+        const resultado = regex.test(email);
+       return resultado;
+    }
 
 
 });
