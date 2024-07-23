ccaf1219f346f054dfb6fd692369f5e5356c8188
javier <inerxd@gmail.com>
2024-07-22 20:17:02 -0600

    js 2.4

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,15 +1,23 @@
+/* Reto Envio de Emails 
+añade un campo extra llamado cc; para añadir un destinatario extra 
+este campo no es obligatorio; pero en caso de tener informacion desbes validar que sea un email valido.
+ */
+
+
 document.addEventListener('DOMContentLoaded', function(){
 
     const email = {
         email: '',
         asunto:'',
-        mensaje:''
+        mensaje:'',
+        cc:''
     };
 
     // seleciona los elementos de la interfaz
     const inputEmail = document.querySelector('#email');
     const inputAsunto = document.querySelector('#asunto');
     const inputMensaje = document.querySelector('#mensaje');
+    const inputCC = document.querySelector('#cc');
     const formulario = document.querySelector('#formulario');
     const btnSubmit = document.querySelector('#formulario button[type="submit"]');
     const btnReset = document.querySelector('#formulario button[type="reset"]');
@@ -18,6 +26,7 @@ document.addEventListener('DOMContentLoaded', function(){
     inputEmail.addEventListener('input',validar);
     inputAsunto.addEventListener('input',validar);
     inputMensaje.addEventListener('input',validar);
+    inputCC.addEventListener('input',emailCC);
 
     formulario.addEventListener('submit',enviarEmail);
 
@@ -62,6 +71,7 @@ document.addEventListener('DOMContentLoaded', function(){
             comprobarEmail();
             return;
             }
+       
      
         if(e.target.id === 'email' && !validarEmail(e.target.value)){
             mostrarAlerta('El email no es valido', e.target.parentElement);
@@ -69,6 +79,9 @@ document.addEventListener('DOMContentLoaded', function(){
             comprobarEmail();
             return;
         }
+        
+    
+
         limpiarAlerta(e.target.parentElement);
         //asignar los valores
         email[e.target.name ] = e.target.value.trim().toLowerCase();
@@ -102,7 +115,7 @@ document.addEventListener('DOMContentLoaded', function(){
     }
 
     function comprobarEmail(){
-        if(Object.values(email).includes('')){
+        if(Object.values(email).includes('') && email.cc === '' ){
             btnSubmit.classList.add('opacity-50');
             btnSubmit.disabled = true;
 
@@ -116,10 +129,27 @@ document.addEventListener('DOMContentLoaded', function(){
         email.email = '';
         email.asunto = '';
         email.mensaje = '';
+        email.cc = '';
+        
 
         formulario.reset();
         comprobarEmail();
     }
 
+    function emailCC(e){
+        if(e.target.value.trim() !== '' && !validarEmail(e.target.value)){
+            mostrarAlerta('El email no es valido', e.target.parentElement);
+            email[e.target.name] = '';
+            comprobarEmail();
+            return;
+        }
+        
+        limpiarAlerta(e.target.parentElement);
+        //asignar los valores
+        email[e.target.name] = e.target.value.trim().toLowerCase();
+        comprobarEmail();
+    }
 
 });
+
+/* Reto Envio de Emails añade un campo*/
